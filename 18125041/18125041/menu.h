be7644399b2e9a62a8babb4fb486db905e442937
nhanlun <39358266+nhanlun@users.noreply.github.com>
2018-11-10 11:57:02 +0700

    final

--- 18125041/18125041/menu.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef menu_h
-#define menu_h
-
-int menu(Data &player);
-
-#endif
--- 18125041/18125041/play.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef play_h
-#define play_h
-
-void game(Data &player, const char s[], int gamemode);
-
-#endif
